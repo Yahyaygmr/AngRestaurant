111ff9bb1821602f1ee65b6b9983e53fadce8010
Yahyaygmr <yahyaygmr@gmail.com>
2025-02-14 20:20:38 +0300

    category service methods created

--- src/app/_services/category.service.ts
+++ src/app/_services/category.service.ts
@@ -7,9 +7,21 @@ import { Category } from '../_models/category';
 })
 export class CategoryService {
   constructor(private http: HttpClient) {}
-  baseUrl = 'https://localhost:7012/api/Categories';
+  baseUrl = 'https://localhost:7012/api/Categories/';
 
   getCategories() {
     return this.http.get<Category[]>(this.baseUrl);
+  }
+  getById(id: number) {
+    return this.http.get<Category[]>(this.baseUrl + id);
+  }
+  create(category: Category) {
+    return this.http.post(this.baseUrl, category);
+  }
+  update(id: number, category: Category) {
+    return this.http.put(this.baseUrl + id, category);
+  }
+  delete(id: number) {
+    return this.http.delete(this.baseUrl + id);
   }
 }
